6359167c9e8d25edf48fc415efa90434a07c8275
Nesat Nayem <nesatnayem042@gmail.com>
2025-12-19 12:23:53 +0600

    domain added

--- next.config.ts
+++ next.config.ts
@@ -14,6 +14,7 @@ const nextConfig: NextConfig = {
       "assets-in.bmscdn.com",
       "lh3.googleusercontent.com", // Google profile images
       "googleusercontent.com",
+      "i.ibb.co.com"
     ],
   },
 };
